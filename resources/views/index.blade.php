d2b9fe3415f4a2971d6661745c5c554951ad3da2
Richard <skynet.tw@gmail.com>
2021-11-24 19:48:16 +0800

    add the base template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,15 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends("includes.base")
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+@section("title")
+我的影片清單
+@endsection
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <div class="container">
-        <h2>我的影音列表</h2>
+@section("titlemessage")
+<h2>何敏煌的影片清單</h2>
+@endsection
+
+@section("maincontent")
+<h2>我的影音列表</h2>
         <hr>
         <form action="/add/" method="POST">
             @csrf
@@ -30,17 +27,4 @@
             </tr>
             @endforeach
         </table>
-    </div>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
+@endsection
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,1 +1,13 @@
+@extends("includes.base")
+
+@section("title")
+我的影音清單
+@endsection
+
+@section("titlemessage")
+{{ $vid }}
+@endsection
+
+@section("maincontent")
 <iframe width="800" height="600" src="https://www.youtube.com/embed/{{ $vid }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+@endsection
